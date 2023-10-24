1f7d73c707839e49a0194669b5a09b9e9a23360b
adarshjaiswal042 <141535327+adarshjaiswal042@users.noreply.github.com>
2023-10-24 18:21:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,11 +26,12 @@ from bs4 import BeautifulSoup
 load_dotenv()
 os.makedirs("./downloads", exist_ok=True)
 
-bot = Client("bot",
-             bot_token= "6265926517:AAFA3vF-F_zhTQRh1dyF-WMbj6quQsQ-xFc",
-             api_id= 8130624,
-             api_hash= "67a71560b00a31ffa692c67428f06d38")
 
+bot = Client(
+    "bot",
+    bot_token=os.environ.get("BOT_TOKEN"),
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH"))
 logger = logging.getLogger()
 # thumb = os.environ.get("THUMB")
 # if thumb.startswith("http://") or thumb.startswith("https://"):
